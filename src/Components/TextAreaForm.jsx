636a8556d59e53c2e99da597b0149d26e1721a2f
Alexey404 <volik.aiii@mail.ru>
2021-06-24 01:34:11 +0300

    last

--- src/Components/TextAreaForm.jsx
+++ src/Components/TextAreaForm.jsx
@@ -6,7 +6,7 @@ export const TextareaForm = () => {
   const [result, setResult] = useState('Hello!')
   const [resultStyle, setResultStyle] = useState({
     fontStyle: 'normal',
-    color: 'red',
+    color: 'black',
     textDecoration: 'none',
     fontWeight: 'normal',
     fontSize: '14px',
@@ -19,76 +19,79 @@ export const TextareaForm = () => {
   }
   return (
     <div>
-      <form onSubmit={() => {}}>
-        <select
-          name="size"
-          id="size"
-          className={styles.size}
-          onChange={(e) => {
-            handleClick('fontSize', e.target.value)
-          }}
-        >
-          <option>10px</option>
-          <option>12px</option>
-          <option>14px</option>
-          <option>16px</option>
-          <option>20px</option>
-        </select>
-        <input
-          id="color"
-          type="color"
-          name="color"
-          className={styles.color}
-          onChange={(e) => {
-            handleClick('color', e.target.value)
-          }}
-        />
-
-        <button
-          id="bold"
-          type="button"
-          name="bold"
-          className={styles.bold}
-          onClick={() => {
-            handleClick('fontWeight', '1000')
-          }}
-        >
-          B
-        </button>
-
-        <button
-          id="cursiv"
-          type="button"
-          name="cursiv"
-          className={styles.cursiv}
-          onClick={() => {
-            handleClick('fontStyle', 'italic')
-          }}
-        >
-          C
-        </button>
-        <button
-          type="button"
-          name="emphatic"
-          className={styles.emphatic}
-          onClick={() => {
-            handleClick('textDecoration', 'underline')
-          }}
-        >
-          E
-        </button>
-        <button type="button" name="link">
-          link
-        </button>
-        <input type="file" name="download" />
-        <div>
-          <textarea
-            defaultValue="type..."
-            onChange={(e) => setResult(e.target.value)}
-            style={resultStyle}
-          />
+      <form style={{ margin: '0 auto' }} onSubmit={() => {}}>
+        <div className={styles.all}>
+          <div className={styles.settings}>
+            <select
+              name="size"
+              id="size"
+              className={styles.size}
+              onChange={(e) => {
+                handleClick('fontSize', e.target.value)
+              }}
+            >
+              <option>10px</option>
+              <option>12px</option>
+              <option>14px</option>
+              <option>16px</option>
+              <option>20px</option>
+            </select>
+            <input
+              id="color"
+              type="color"
+              name="color"
+              className={styles.color}
+              onChange={(e) => {
+                handleClick('color', e.target.value)
+              }}
+            />
+            <button
+              id="bold"
+              type="button"
+              name="bold"
+              className={styles.bold}
+              onClick={() => {
+                handleClick('fontWeight', '1000')
+              }}
+            >
+              B
+            </button>
+            <button
+              id="cursiv"
+              type="button"
+              name="cursiv"
+              className={styles.cursiv}
+              onClick={() => {
+                handleClick('fontStyle', 'italic')
+              }}
+            >
+              C
+            </button>
+            <button
+              type="button"
+              name="emphatic"
+              className={styles.emphatic}
+              onClick={() => {
+                handleClick('textDecoration', 'underline')
+              }}
+            >
+              E
+            </button>
+            <button type="button" name="link">
+              link
+            </button>
+            <input type="file" name="download" />{' '}
+          </div>
+          <div>
+            <textarea
+              className={styles.textarea}
+              onChange={(e) => setResult(e.target.value)}
+              style={resultStyle}
+              placeholder={'type...'}
+            />
+            <button type="submit">Submit</button>
+          </div>
         </div>
-        <button type="submit">Submit</button>
       </form>
       <RenderResultArea text={result} style={{ ...resultStyle }} />
     </div>
