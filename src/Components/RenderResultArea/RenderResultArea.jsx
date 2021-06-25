0eaaf6cc3ff990303b149add1a02159cf42ad7fb
Alexey404 <volik.aiii@mail.ru>
2021-06-26 01:35:28 +0300

    last

--- src/Components/RenderResultArea/RenderResultArea.jsx
+++ src/Components/RenderResultArea/RenderResultArea.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
+import React from 'react'
 import styles from './renderarea.module.css'
 
-const RenderResultArea =(props)=> {
-
-    return (
-        <div className={styles.area}>
-       <h1>Result</h1>
-       <div className={styles.text} style={props.style}>
-           {props.text}
-       </div>
-       </div> 
-         
-  
-    )
+const RenderResultArea = (props) => {
+  return (
+    <div className={styles.area}>
+      <h1>Result</h1>
+      <div className={styles.text} style={props.style}>
+        {props.text}
+      </div>
+    </div>
+  )
 }
 
 export default RenderResultArea
--- src/Components/TextAreaForm.jsx
+++ src/Components/TextAreaForm.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 
 export const TextareaForm = () => {
   const [result, setResult] = useState('Hello!')
+  const [isStyle, setIsStyle] = useState(false)
   const [resultStyle, setResultStyle] = useState({
     fontStyle: 'normal',
     color: 'black',
@@ -17,80 +18,97 @@ export const TextareaForm = () => {
   const handleClick = (prop, value) => {
     setResultStyle({ ...resultStyle, [prop]: value })
   }
+
+  const handlerSettings = () => {
+    setIsStyle(!isStyle)
+  }
+
+  const display = isStyle ? 'block' : 'none'
+
   return (
     <div>
       <form style={{ margin: '0 auto' }} onSubmit={() => {}}>
         <div className={styles.all}>
-          <div className={styles.settings}>
-            <select
-              name="size"
-              id="size"
-              className={styles.size}
-              onChange={(e) => {
-                handleClick('fontSize', e.target.value)
-              }}
-            >
-              <option>10px</option>
-              <option>12px</option>
-              <option>14px</option>
-              <option>16px</option>
-              <option>20px</option>
-            </select>
-            <input
-              id="color"
-              type="color"
-              name="color"
-              className={styles.color}
-              onChange={(e) => {
-                handleClick('color', e.target.value)
-              }}
-            />
-            <button
-              id="bold"
-              type="button"
-              name="bold"
-              className={styles.bold}
-              onClick={() => {
-                handleClick('fontWeight', '1000')
-              }}
-            >
-              B
-            </button>
-            <button
-              id="cursiv"
-              type="button"
-              name="cursiv"
-              className={styles.cursiv}
-              onClick={() => {
-                handleClick('fontStyle', 'italic')
-              }}
-            >
-              C
-            </button>
-            <button
-              type="button"
-              name="emphatic"
-              className={styles.emphatic}
-              onClick={() => {
-                handleClick('textDecoration', 'underline')
-              }}
-            >
-              E
-            </button>
-            <button type="button" name="link">
-              link
-            </button>
-            <input type="file" name="download" />{' '}
-          </div>
-          <div>
-            <textarea
-              className={styles.textarea}
-              onChange={(e) => setResult(e.target.value)}
-              style={resultStyle}
-              placeholder={'type...'}
-            />
+          <span>
+            <span>
+              <div className={styles.settings}>
+                <div style={{ display }}>
+                  <select
+                    name="size"
+                    id="size"
+                    className={styles.size}
+                    onChange={(e) => {
+                      handleClick('fontSize', e.target.value)
+                    }}
+                  >
+                    <option>10px</option>
+                    <option>12px</option>
+                    <option>14px</option>
+                    <option>16px</option>
+                    <option>20px</option>
+                  </select>
+                  <input
+                    id="color"
+                    type="color"
+                    name="color"
+                    className={styles.color}
+                    onChange={(e) => {
+                      handleClick('color', e.target.value)
+                    }}
+                  />
+                  <button
+                    id="bold"
+                    type="button"
+                    name="bold"
+                    className={styles.bold}
+                    onClick={() => {
+                      handleClick('fontWeight', '1000')
+                    }}
+                  >
+                    B
+                  </button>
+                  <button
+                    id="cursiv"
+                    type="button"
+                    name="cursiv"
+                    className={styles.cursiv}
+                    onClick={() => {
+                      handleClick('fontStyle', 'italic')
+                    }}
+                  >
+                    C
+                  </button>
+                  <button
+                    type="button"
+                    name="emphatic"
+                    className={styles.emphatic}
+                    onClick={() => {
+                      handleClick('textDecoration', 'underline')
+                    }}
+                  >
+                    E
+                  </button>
+                  <button type="button" name="link">
+                    link
+                  </button>
+                </div>
+                <button
+                  type="button"
+                  style={{ float: 'right' }}
+                  onClick={handlerSettings}
+                >
+                  {isStyle ? '<' : '>'}
+                </button>
+              </div>
+              <textarea
+                className={styles.textarea}
+                onChange={(e) => setResult(e.target.value)}
+                style={resultStyle}
+                placeholder={'type...'}
+              />
+            </span>
             <button type="submit">Submit</button>
-          </div>
+          </span>
         </div>
       </form>
       <RenderResultArea text={result} style={{ ...resultStyle }} />
