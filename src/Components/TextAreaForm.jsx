b12fded9824cbb51224e5768f05cd1ec4e2bd818
Alexey404 <volik.aiii@mail.ru>
2021-06-28 01:19:12 +0300

    Тестим

--- src/Components/TextAreaForm.jsx
+++ src/Components/TextAreaForm.jsx
@@ -3,31 +3,29 @@ import RenderResultArea from './RenderResultArea/RenderResultArea'
 import { useState } from 'react'
 
 export const TextareaForm = () => {
-
   const textStyle = {
-    fontStyle: "normal",
-    color: "black",
-    textDecoration: "none",
-    fontWeight: "normal",
-    fontSize: "14px",
+    fontStyle: 'normal',
+    color: 'black',
+    textDecoration: 'none',
+    fontWeight: 'normal',
+    fontSize: '14px',
     // link: false
-  };
-  const [result, setResult] = useState("Hello!");
-  const [resultStyle, setResultStyle] = useState(textStyle);
+  }
+  const [result, setResult] = useState('Hello!')
+  const [resultStyle, setResultStyle] = useState(textStyle)
   const [isStyle, setIsStyle] = useState(false)
   const handle = (prop, value, event) => {
-    setResultStyle({ ...resultStyle, [prop]: value });
+    setResultStyle({ ...resultStyle, [prop]: value })
     resultStyle[prop] !== textStyle[prop] &&
       !event &&
-     setResultStyle( {...resultStyle, [prop]: textStyle[prop] }
-    )}
+      setResultStyle({ ...resultStyle, [prop]: textStyle[prop] })
+  }
 
-    const handlerSettings = () => {
-      setIsStyle(!isStyle)
-    }
-  
-    const display = isStyle ? 'block' : 'none'
-  
+  const handlerSettings = () => {
+    setIsStyle(!isStyle)
+  }
+
+  const display = isStyle ? 'block' : 'none'
 
   return (
     <div>
