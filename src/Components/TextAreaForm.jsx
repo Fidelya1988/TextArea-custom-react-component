967f99f4f3e7e93931122b3370dfafa4de5481c1
Fidelya1988 <oleksandram1988@gmail.com>
2021-06-28 21:16:09 +0300

    заменила код кнопок на модуль TextstyleButtons

--- src/Components/TextAreaForm.jsx
+++ src/Components/TextAreaForm.jsx
@@ -1,6 +1,7 @@
 import styles from './textArea.module.css'
 import RenderResultArea from './RenderResultArea/RenderResultArea'
 import { useState } from 'react'
+import { TextStyleButtons } from './TextStylesButtons'
 
 export const TextareaForm = () => {
   const textStyle = {
@@ -33,66 +34,8 @@ export const TextareaForm = () => {
       <form style={{ margin: '0 auto' }} onSubmit={() => {}}>
         <div className={styles.all}>
           <span>
-            <div className={styles.settings} style={{ display }}>
-              <select
-                name="size"
-                id="size"
-                className={styles.btn}
-                onChange={(e) => {
-                  handle('fontSize', e.target.value)
-                }}
-              >
-                <option>10px</option>
-                <option>12px</option>
-                <option>14px</option>
-                <option>16px</option>
-                <option>20px</option>
-              </select>
-              <input
-                id="color"
-                type="color"
-                name="color"
-                className={styles.btn}
-                onChange={(e) => {
-                  handle('color', e.target.value)
-                }}
-              />
-              <button
-                id="bold"
-                type="button"
-                name="bold"
-                className={styles.btn}
-                onClick={() => {
-                  handle('fontWeight', '1000')
-                }}
-              >
-                B
-              </button>
-              <button
-                id="cursiv"
-                type="button"
-                name="cursiv"
-                className={styles.btn}
-                onClick={() => {
-                  handle('fontStyle', 'italic')
-                }}
-              >
-                C
-              </button>
-              <button
-                type="button"
-                name="emphatic"
-                className={styles.btn}
-                onClick={() => {
-                  handle('textDecoration', 'underline')
-                }}
-              >
-                E
-              </button>
-              <button className={styles.btn} type="button" name="link">
-                link
-              </button>
-            </div>
+            <TextStyleButtons handle = {handle} display={display}/>
+           
             <button
               type="button"
               className={styles.btnSettings}
