a3e8f7ee4b823187de7e1803a2826ad8008e54b4
Fidelya1988 <oleksandram1988@gmail.com>
2021-06-30 01:18:52 +0300

    отрисовала компоненту HideShowButton

--- src/Components/TextAreaForm.jsx
+++ src/Components/TextAreaForm.jsx
@@ -1,54 +1,53 @@
-import styles from './textArea.module.css'
-import RenderResultArea from './RenderResultArea/RenderResultArea'
-import { useState } from 'react'
-import { TextStyleButtons } from './TextStylesButtons'
+import styles from "./textArea.module.css";
+import RenderResultArea from "./RenderResultArea/RenderResultArea";
+import { useState } from "react";
+import { TextStyleButtons } from "./TextStylesButtons";
+import { HideShowButton } from "./HideShowButton";
 
 export const TextareaForm = () => {
   const textStyle = {
-    fontStyle: 'normal',
-    color: 'black',
-    textDecoration: 'none',
-    fontWeight: 'normal',
-    fontSize: '14px',
+    fontStyle: "normal",
+    color: "black",
+    textDecoration: "none",
+    fontWeight: "normal",
+    fontSize: "14px",
     // link: false
-  }
-  const [result, setResult] = useState('Hello!')
-  const [resultStyle, setResultStyle] = useState(textStyle)
-  const [isStyle, setIsStyle] = useState(false)
+  };
+  const [result, setResult] = useState("Hello!");
+  const [resultStyle, setResultStyle] = useState(textStyle);
+  const [isStyle, setIsStyle] = useState(false);
+
   const handle = (prop, value, event) => {
-    setResultStyle({ ...resultStyle, [prop]: value })
+    setResultStyle({ ...resultStyle, [prop]: value });
     resultStyle[prop] !== textStyle[prop] &&
       !event &&
-      setResultStyle({ ...resultStyle, [prop]: textStyle[prop] })
-  }
+      setResultStyle({ ...resultStyle, [prop]: textStyle[prop] });
+  };
 
   const handlerSettings = () => {
-    setIsStyle(!isStyle)
-  }
+    setIsStyle(!isStyle);
+  };
 
-  const display = isStyle ? 'flex' : 'none'
-  const backgroundColor = !isStyle ? '' : 'rgba(0, 0, 0, 0.288)'
+  const display = isStyle ? "flex" : "none";
+  const backgroundColor = !isStyle ? "" : "rgba(0, 0, 0, 0.288)";
 
   return (
     <div className={styles.container}>
-      <form style={{ margin: '0 auto' }} onSubmit={() => {}}>
+      <form style={{ margin: "0 auto" }} onSubmit={() => {}}>
         <div className={styles.all}>
           <span>
-            <TextStyleButtons handle = {handle} display={display}/>
-           
-            <button
-              type="button"
-              className={styles.btnSettings}
-              onClick={handlerSettings}
-              style={{ backgroundColor }}
-            >
-              {isStyle ? '˅' : '˄'}
-            </button>
+            <TextStyleButtons handle={handle} display={display} />
+            <HideShowButton
+              backgroundColor={backgroundColor}
+              isStyle={isStyle}
+              handlerSettings={handlerSettings}
+            />
+
             <textarea
               className={styles.textarea}
               onChange={(e) => setResult(e.target.value)}
               style={resultStyle}
-              placeholder={'type...'}
+              placeholder={"type..."}
             />
 
             <button type="submit">Submit</button>
@@ -57,5 +56,5 @@ export const TextareaForm = () => {
         <RenderResultArea text={result} style={{ ...resultStyle }} />
       </form>
     </div>
-  )
-}
+  );
+};
